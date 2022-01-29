b82aaf75e52d2760ac260010cbd81b0644251847
Btaft6278 <btaft6278@gmail.com>
2022-01-28 20:32:06 -0500

    connected mongodb

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
+require('dotenv').config();
+
+var corsOptions= {
+    origin: 'http://127.0.0.1:8000'
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/',(req, res) => {
+    res.json({message: "Hello World"});
+});
 
-app.listen(8000, () => {
-    console.log('Server Is Running...')
-})
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Server Is Running in ${PORT}...`);
+});
