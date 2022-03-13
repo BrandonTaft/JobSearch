74267a91f1e2a3fa1f595e513f0417cf0b4a4079
Btaft6278 <btaft6278@gmail.com>
2022-03-12 21:33:51 -0500

    eod

--- Client/src/pages/Login.js
+++ Client/src/pages/Login.js
@@ -1,102 +1,62 @@
 import style from "../css/login.module.css";
-import { useEffect, useState } from 'react';
-import googleLogo from '../img/google.png';
-import { loadGoogleScript } from '../middleware/GoogleLogIn';
-const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+import React, { useReducer, useState } from 'react';
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+ }
 
 function Login() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+   setSubmitting(true);
+
+   setTimeout(() => {
+     setSubmitting(false);
+   }, 3000)
+ }
+ 
+ const handleChange = event => {
+  setFormData({
+    name: event.target.name,
+    value: event.target.value,
+  });
+}
+
   
-  const [gapi, setGapi] = useState();
-  const [googleAuth, setGoogleAuth] = useState();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [imageUrl, setImageUrl] = useState();
-  
-  const onSuccess = (googleUser) => {
-    setIsLoggedIn(true);
-    const profile = googleUser.getBasicProfile();
-    setName(profile.getName());
-    setEmail(profile.getEmail());
-    setImageUrl(profile.getImageUrl());
-  };
-  
-  const onFailure = () => {
-    setIsLoggedIn(false);
-  }
-  
-  const logOut = () => {
-    (async() => {
-      await googleAuth.signOut();
-      setIsLoggedIn(false);
-      renderSigninButton(gapi);
-    })();
-  };
-  
-  const renderSigninButton = (_gapi) => {
-    _gapi.signin2.render('google-signin', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSuccess,
-      'onfailure': onFailure 
-    });
-  }
-  
-  
-  useEffect(() => {
-    
-    //window.gapi is available at this point
-    window.onGoogleScriptLoad = () => {
-     
-      const _gapi = window.gapi;
-      setGapi(_gapi);
-      
-      _gapi.load('auth2', () => {
-        (async () => { 
-          const _googleAuth = await _gapi.auth2.init({
-           client_id: googleClientId
-          });
-          setGoogleAuth(_googleAuth);
-          renderSigninButton(_gapi);
-        })();
-      });
+    return(
+        <div className={style.wrapper}>
+          <h1>How About Them Apples</h1>
+          {/* {submitting &&
+       <div>Submtting Form...</div>
+     } */}
+     {submitting &&
+       <div>
+         You are submitting the following:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+      }
+          <form onSubmit={handleSubmit}>
+          <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" onChange={handleChange}/>
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+      )
     }
     
-    //ensure everything is set before loading the script
-    loadGoogleScript();
-    
-  }, []);
-  
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-      <div >
-                <a href="http://127.0.0.1:8001/auth/google">
-                    <img id="icon" src={googleLogo} alt="Login With Google" ></img>
-                </a>
-                </div>        
-                
-        {!isLoggedIn &&
-          <div id="google-signin"></div>
-        }
-         
-        {isLoggedIn &&
-          <div>
-            <div>
-              <img src={imageUrl} />
-            </div>
-            <div>{name}</div>
-            <div>{email}</div>
-            <button className='btn-primary' onClick={logOut}>Log Out</button>
-          </div>
-        }
-      </header>
-    </div>
-  );
-}
 
 export default Login
